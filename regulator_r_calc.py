7bf6431c80a9ad5208863466158a4f01b68d48b8
eggfly <lihaohua90@gmail.com>
2024-09-18 23:18:48 +0800

    Refactor

--- regulator_r_calc.py
+++ regulator_r_calc.py
@@ -3,10 +3,10 @@ import json
 import time
 import re
 
-def get_jlc_session(packaging: str, pageNum: int):
-    s = requests.Session()  
-    # noinspection SpellCheckingInspection  
-    s.headers = {  
+def get_jlc_session(packaging: str, pageNum: int, isFreeExtendLib: bool):
+    s = requests.Session()
+    # noinspection SpellCheckingInspection
+    s.headers = {
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',  
         'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',  
         'sec-ch-ua-platform': '"macOS"',  
@@ -27,20 +27,25 @@ def get_jlc_session(packaging: str, pageNum: int):
     }  
 
     url = 'https://www.jlcsmt.com/api/smtComponentOrder/componentSearchController/selectPasteComponentList'  
-
-    body = '''{"ascFlag":true,"baseQueryDto":{"componentBrandList":[],"componentSpecificationList":["0603"],"componentTypeIdList":[439],"preferredComponentFlagList":["false"],"orderLibraryTypeList":["base"],"packageTypeList":[],"productTypeIdList":[308],"inStockFlag":false},"groupFlag":false,"pageVo":{"pageNum":3,"pageSize":10},"paramDtoList":[],"queryString":"","sortMode":"COMPREHENSIVE"}'''
+    # ascFlag -> ?
+    body = ''
+    if isFreeExtendLib:
+        # 合作优选，扩展库
+        body = '''{"ascFlag":false,"baseQueryDto":{"componentBrandList":[],"componentSpecificationList":["0402"],"componentTypeIdList":[439],"preferredComponentFlagList":["true"],"orderLibraryTypeList":["expand"],"packageTypeList":[],"productTypeIdList":[308]},"groupFlag":false,"pageVo":{"pageNum":1,"pageSize":10},"paramDtoList":[],"queryString":"","sortMode":"COMPREHENSIVE"}'''
+    else:
+        body = '''{"ascFlag":true,"baseQueryDto":{"componentBrandList":[],"componentSpecificationList":["0603"],"componentTypeIdList":[439],"preferredComponentFlagList":["false"],"orderLibraryTypeList":["base"],"packageTypeList":[],"productTypeIdList":[308],"inStockFlag":false},"groupFlag":false,"pageVo":{"pageNum":1,"pageSize":10},"paramDtoList":[],"queryString":"","sortMode":"COMPREHENSIVE"}'''
     body = body.encode('utf-8')
     body = json.loads(body)
     body['pageVo']['pageNum'] = pageNum
     # body['queryString'] = "Ω"
-    body['baseQueryDto']['componentSpecificationList'][0] = packaging
+    body['baseQueryDto']['componentSpecificationList'][0] = packaging # 0603, 0402, 0805...
     return s, url, body
 
-def get_component_list(packaging: str):
+def get_component_list(packaging: str, isFreeExtendLib: bool):
     results = []
     pageNum = 1
     while True:
-        s, url, body = get_jlc_session(packaging, pageNum)
+        s, url, body = get_jlc_session(packaging, pageNum, isFreeExtendLib)
         response = s.post(url, json=body)
         print('pageNum:', pageNum)
         pageNum += 1
@@ -66,8 +71,10 @@ def query_from_jlc():
     results = []
     # results += get_component_list('1206')
     # results += get_component_list('0805')
-    results += get_component_list('0603')
-    results += get_component_list('0402')
+    results += get_component_list('0402', False)
+    results += get_component_list('0402', True)
+    # results += get_component_list('0603', False)
+    # results += get_component_list('0603', True)
     return results
 
 def extract_resistance_values(text):  
@@ -92,10 +99,11 @@ def parse_result(data_list):
     for item in data_list:  
         name = item['componentName']  
         code = item['componentCode']
+        spec = item['componentSpecification']
         # print(name)
         if 'Ω' in name:
             r_val, full_str = extract_resistance_values(name)
-            info = {'value': r_val, 'code':code, 'full': full_str}
+            info = {'value': r_val, 'code':code, 'full': full_str, 'spec': spec}
             values.append(info)
             print(info)
     print('value count:', len(values))
@@ -103,7 +111,7 @@ def parse_result(data_list):
 
 
 target_v_out = 5.1
-v_error = 0.4
+v_error = 0.3
 v_ref = 0.6
 
 def match_extra(r2):
@@ -115,10 +123,12 @@ def print_matched_result(values):
         r1 = v1['value']
         code1 = v1['code']
         full_str1 = v1['full']
+        spec1 = v1['spec']
         for v2 in values:
             r2 = v2['value']
             code2 = v2['code']
             full_str2 = v2['full']
+            spec2 = v2['spec']
             if r2 == 0:
                 continue
             v_out = v_out_calc(r1, r2, v_ref)
@@ -126,7 +136,7 @@ def print_matched_result(values):
                 feedback_current = v_out / (r1 + r2) * 1000 * 1000
                 # noinspection SpellCheckingInspection
                 print(
-                    "R1 = {:.0f}Ω {} {}, R2 = {:.0f}Ω {} {}, Vout = {:.3f} V, Current = {:.3f} μA".format(r1, code1, full_str1, r2, code2, full_str2,
+                    "R1 = {:.0f}Ω {} {}({}), R2 = {:.0f}Ω {} {}({}), Vout = {:.3f} V, Current = {:.3f} μA".format(r1, code1, full_str1, spec1, r2, code2, full_str2, spec2,
                                                                                                     v_out, feedback_current))
                 total += 1
     print("Found %s value pair!" % total)
